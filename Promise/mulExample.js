51f86141e956f350f0e02770af24465ad25be3bd
Dovey <172399463@qq.com>
2018-12-27 18:00:15 +0800

    学习高阶函数

--- Promise/mulExample.js
+++ Promise/mulExample.js
@@ -1,60 +1,86 @@
-function runAsync1(){
+// function runAsync1(){
 
-    var p = new Promise((resolve,reject)=>{
+//     var p = new Promise((resolve,reject)=>{
         
-        let data = 'initData 1';
-        setTimeout(()=>{
-            console.log('执行完成')
-            data = 'renAsync 1'
-            resolve(data)
-        },2000)
+//         let data = 'initData 1';
+//         setTimeout(()=>{
+//             console.log('执行完成')
+//             data = 'renAsync 1'
+//             resolve(data)
+//         },2000)
 
-    })
-    console.log(p)
-    return p;
+//     })
+//     console.log(p)
+//     return p;
+// }
+
+// function runAsync2(){
+
+//     var p = new Promise((resolve,reject)=>{
+        
+//         let data = '初始化数据2';
+//         setTimeout(()=>{
+//             console.log('执行完成')
+//             data = 'renAsync2'
+//             resolve(data)
+//         },2000)
+
+//     })
+//     console.log(p)
+//     return p;
+// }
+
+// function runAsync3(){
+
+//     var p = new Promise((resolve,reject)=>{
+        
+//         let data = '初始化数据3';
+//         setTimeout(()=>{
+//             console.log('执行完成')
+//             data = 'renAsync3'
+//             resolve(data)
+//         },2000)
+
+//     })
+//     console.log(p)
+//     return p;
+// }
+
+// runAsync1()
+// .then(function(data){
+//     console.log(data);
+//     return runAsync2();
+// })
+// .then(function(data){
+//     console.log(data);
+//     return runAsync3();
+// })
+// .then(function(data){
+//     console.log(data);
+// });
+
+function getURL(URL) {
+    return new Promise(function (resolve, reject) {
+        var req = new XMLHttpRequest();
+        req.open('GET', URL, true);
+        req.onload = function () {
+            if (req.status === 200) {
+                resolve(req.responseText);
+            } else {
+                reject(new Error(req.statusText));
+            }
+        };
+        req.onerror = function () {
+            reject(new Error(req.statusText));
+        };
+        req.send();
+    });
 }
 
-function runAsync2(){
-
-    var p = new Promise((resolve,reject)=>{
-        
-        let data = '初始化数据2';
-        setTimeout(()=>{
-            console.log('执行完成')
-            data = 'renAsync2'
-            resolve(data)
-        },2000)
-
-    })
-    console.log(p)
-    return p;
-}
-
-function runAsync3(){
-
-    var p = new Promise((resolve,reject)=>{
-        
-        let data = '初始化数据3';
-        setTimeout(()=>{
-            console.log('执行完成')
-            data = 'renAsync3'
-            resolve(data)
-        },2000)
-
-    })
-    console.log(p)
-    return p;
-}
-
-runAsync1()
-.then(function(data){
-    console.log(data);
-    return runAsync2();
-})
-.then(function(data){
-    console.log(data);
-    return runAsync3();
-})
-.then(function(data){
-    console.log(data);
+// 运行请求实例
+var  URL = 'http://httpbin.org/get'
+getURL(URL).then(function onFulFilled(value){
+    console.log(value)
+}).catch(function onRejected(error){
+    console.error(error)
 });
--- Promise/simple.js
+++ Promise/simple.js
@@ -1,22 +1,49 @@
-function renAsync(){
+// function renAsync(){
 
-    var p = new Promise((resolve,reject)=>{
+//     // 返回一个promise对象    
+//     var p = new Promise((resolve,reject)=>{
         
-        let data = '初始化数据';
-        setTimeout(()=>{
-            console.log('执行完成')
-            data = '新数据'
-            resolve(data)
-        },2000)
+//         let data = '初始化数据';
+//         setTimeout(()=>{
+//             console.log('执行完成')
+//             data = '新数据'
+//             resolve(data)
+//         },2000)
 
-    })
-    console.log(p)
-    return p;
-}
+//     })
+//     console.log(p)
+//     return p;
+// }
 
-renAsync().then(data=>{
-    console.log(data)
+// renAsync().then(data=>{
+//     console.log(data)
+// })
+
+
+var promise = new Promise(function (resolve,reject){
+    // 异步处理
+
+    // 处理结束之后，调用resolve或reject
+    throw 'cuowula'
+
+})
+
+promise.then(value=>{
+    console.log(value)
+},error=>{
+    console.log('error:')
+    console.error(error)
 })
 
 
+// Promise 的三种状态
+/*
+    1 Fulfilled 成功时调用，
+    2 Rejected 失败时调用
+    3 unresolved promise对象刚被创建之后的初始化状态
+
+    
+*/
+
+// new Promise()
 
